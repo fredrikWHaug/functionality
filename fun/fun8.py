4da28ac4267cd7b459ce235f5239ae51b50796d4
Fredrik Willumsen Haug <fredrikwhaug@gmail.com>
2023-09-22 03:18:09 -0400

    Update fun8.py

--- fun/fun8.py
+++ fun/fun8.py
@@ -1,9 +1,11 @@
+# function squaring every other element in a list
 def every_other_is_squared(nums):
     for i in range(len(nums)):
         if (i % 2 == 0):
             nums[i] = nums[i] ** 2
     return nums
 
+#example run
 if __name__ == '__main__':
     numsi = [1, 2, 3, 4]
     print(every_other_is_squared(numsi))
