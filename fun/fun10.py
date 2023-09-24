1151e0c030fd1505c984a2773cdce5b77046a6d3
Fredrik Willumsen Haug <fredrikwhaug@gmail.com>
2023-09-24 01:44:32 -0400

    Create fun10.py

---
+++ fun/fun10.py
@@ -0,0 +1,7 @@
+
+def ten_day_celebration(number_range):
+    for i in range(number_range):
+        print('I have been codining for 10 days');
+
+if __name__ == '__main__':
+    ten_day_celebration(10)
