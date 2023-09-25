733e41fc8a3b86e0ddc9f27feda04d0b41dac15a
Fredrik Willumsen Haug <fredrikwhaug@gmail.com>
2023-09-25 04:49:30 -0400

    Create fun11.py

---
+++ fun/fun11.py
@@ -0,0 +1,9 @@
+# interpreter example
+def interpreter():
+    the_world_is_flat = True
+    if the_world_is_flat:
+        print("Be careful not to fall off")
+
+# main execution
+if __name__ == "__main__":
+    interpreter()
