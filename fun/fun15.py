881750f688fd4437d078010bb85fb84133bc733f
Fredrik Willumsen Haug <fredrikwhaug@gmail.com>
2023-09-29 07:29:12 -0400

    Create fun15.py

---
+++ fun/fun15.py
@@ -0,0 +1,11 @@
+
+# merge lists function decleration
+def merge(list1, list2):
+    list3 = [list1, list2]
+    return list3
+
+# main block execution
+if __name__ == '__main__':
+    l1 = [1, 2, 3]
+    l2 = ['a', 'b', 'c']
+    print(merge(l1, l2))
