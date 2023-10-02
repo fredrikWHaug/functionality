90b55c3fd18af006dc9d42c7dbaac0c4202da054
Fredrik Willumsen Haug <fredrikwhaug@gmail.com>
2023-10-02 08:49:39 -0400

    robustness poem

---
+++ fun/week3/fun18.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+# every thoght you have 
+# every interaction you make
+# must be robust (implementation)
+def robustness(x, y):
+    sum = 0
+    sum += (np.sqrt(x) + np.sqrt(y));
+    return sum
+
+# main execution
+if __name__ == '__main__':
+    x = int(input("Give me an x: "));
+    y = int(input('Give me a y: '));
+    print(f"Here is the output of the function with your specifications: {robustness(x, y)}")
