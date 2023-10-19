52ad811f37f7442368169a176f8873cbdd30948c
Fredrik Willumsen Haug <fredrikwhaug@gmail.com>
2023-10-19 08:43:12 -0400

    reday

--- fun/week5/fun34.py
+++ fun/week5/fun34.py
@@ -1,14 +1,14 @@
 
-# fibonacci function with filling list
-def fib2(n):
+# fib function with list return
+def fib(n):
     result = []
     a, b = 0, 1
     while a < n:
         result.append(a)
-        a, b = b, a + b
+        a, b = b, a+b
     return result
 
 # main execution
 if __name__ == '__main__':
-    fib_list = fib2(14)
+    fib_list = fib(1000)
     print(fib_list)
