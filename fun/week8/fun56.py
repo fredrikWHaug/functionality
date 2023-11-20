37b90af2361165822c2423a155af302957f135b2
Fredrik Willumsen Haug <fredrikwhaug@gmail.com>
2023-11-20 13:20:58 -0500

    chat GPT instructed mario... honestly very interesting :(

---
+++ fun/week8/fun56.py
@@ -0,0 +1,27 @@
+# mario script
+
+# get height function
+def get_height():
+    while True:
+        try:
+            height = int(input('Enter an integer between 1 and 8: '))
+            if 1 <= height <= 8:
+                return height
+        except ValueError:
+            pass
+        print('Please enter an integer between 1 and 8')
+
+# print right aligned pyramid
+def print_pyramid(height):
+    for i in range(height):
+        spaces = ' ' * (height - i - 1)
+        hashes = '#' * (i + 1)
+        print(spaces + hashes)
+
+# main function
+def main():
+    height = get_height()
+    print_pyramid(height)
+
+if __name__ == '__main__':
+     main()
