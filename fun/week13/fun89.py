983779d2ed2654064436d9ca54ee2d571db10482
Fredrik Willumsen Haug <fredrikwhaug@gmail.com>
2023-12-18 08:08:03 -0500

    Monday Recursion

---
+++ fun/week13/fun89.py
@@ -0,0 +1,18 @@
+# recursion fib
+
+# fib function
+def fib(n):
+    if n <= 1:
+        return n
+    else:
+        return fib(n - 1) + fib(n - 2)
+
+
+# main function
+def main():
+    for i in range(10):
+        print(fib(i))
+
+# main execution
+if __name__ == '__main__':
+    main()
