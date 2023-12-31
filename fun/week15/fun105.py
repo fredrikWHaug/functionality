6dbe1da32f7de4e7eb861658df2e186d83abc1bc
Fredrik Willumsen Haug <fredrikwhaug@gmail.com>
2023-12-31 08:26:54 +0100

    Sunday binary search

---
+++ fun/week15/fun105.py
@@ -0,0 +1,30 @@
+# binary search
+
+def binary_search(target, array):
+    low, high = 0, len(array) - 1
+    while low <= high:
+        mid = (low + high) // 2
+        mid_value = array[mid]
+
+        if mid_value == target:
+            return mid
+        elif mid_value < target:
+            low = mid + 1
+        else:
+            high = low - 1
+    return  - 1
+
+def main():
+    ordered_list = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+    target = 5
+
+    result = binary_search(ordered_list, target)
+    
+    if result != -1:
+        print(f'Target found at index {result}')
+    else:
+        print(f'Target not found in given list')
+
+# main execution
+if __name__ == '__main__':
+    main()
