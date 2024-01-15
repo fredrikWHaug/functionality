bb4f211abbdcc5df00ab5356e784907e1ebb87c5
Fredrik Willumsen Haug <fredrikwhaug@gmail.com>
2024-01-15 09:11:49 -0500

    Monday fib

---
+++ fun/fun24/week3/fib3.py
@@ -0,0 +1,29 @@
+# fibonacci
+
+# fib with recursion
+def fibr(n):
+    if n <= 1:
+        return n
+    else:
+        return fibr(n - 1) + fibr(n - 2)
+
+# fib without recursion
+def fib(n):
+    result = []
+    a, b = 0, 1
+    while a < n:
+        result.append(a)
+        a, b = b, a + b
+    return result
+
+# main function
+def main():
+    print(fib(100))
+    result = []
+    for i in range(12):
+        result.append(fibr(i))
+    print(result)
+
+# main execution
+if __name__ == '__main__':
+    main()
