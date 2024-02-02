db36ba00504d7f5f652a8b51f354a95980d9063d
Fredrik Willumsen Haug <fredrikwhaug@gmail.com>
2024-02-02 13:02:16 -0500

    Friday Binary search

---
+++ fun/fun24/week5/binary_search5.py
@@ -0,0 +1,21 @@
+# binary search
+
+def binary_search(target, array):
+    low, high = 0, len(array) - 1
+    while low < high:
+        mid = (low + high) // 2
+        mid_value = array[mid]
+        if target == mid_value:
+            return mid
+        elif target > mid_value:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return mid
+
+# main function
+def main():
+    my_list = [1, 2, 3, 4]
+    target = 2
+    index = binary_search(target, my_list)
+    print(index)
