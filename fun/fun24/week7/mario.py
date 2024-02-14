76212972b5f29632ec5f047ba4dd8b28b97ef926
Fredrik Willumsen Haug <fredrikwhaug@gmail.com>
2024-02-14 11:13:28 -0500

    Complete

--- fun/fun24/week7/mario.py
+++ fun/fun24/week7/mario.py
@@ -1,14 +1,29 @@
+# mario
 
+# get height from user
 def get_height():
     while True:
-        height = int(input('Please enter an integer between 1 and 8: '))
-        if 1 <= height <= 8:
-            return height
-    except ValueError:
+        try:
+            height = int(input('Please enter an integer between 1 and 8: '))
+            if 1 <= height <= 8:
+                return height
+        except ValueError:
+            pass
+        print('Please enter an integer between 1 and 8')
 
 
+# print pyramid
 def print_pyramid(height):
     for i in range(height):
         spaces = ' ' * (height - i - 1)
         hashes = '#' * (i + 1)
         print(spaces + hashes)
+
+# main function
+def main():
+    height = get_height()
+    print_pyramid(height)
+
+# main execution
+if __name__ == '__main__':
+    main()
