60c4d344630475b5da0f6e1db1b428077e195f2b
Fredrik Willumsen Haug <fredrikwhaug@gmail.com>
2024-03-03 12:01:30 -0500

    fib rep

---
+++ fun/fun24/week8/fibn.py
@@ -0,0 +1,14 @@
+
+def fibn(n):
+    result = []
+    a, b = 0, 1
+    while a <= n:
+        result.append(a)
+        a, b = b, a + b
+    return result
+
+def main():
+    print(fibn(10))
+
+if __name__ == '__main__':
+    main()
